c9763bbecd9ccd701862d021089b4645605efbfc
Arnav Dadarya <37542991+ardada2468@users.noreply.github.com>
2023-01-07 15:07:03 -0800

    asdfa

--- build/lib/pystuff/test.py
+++ build/lib/pystuff/test.py
@@ -1,4 +1,15 @@
 import argparse
+import requests
+
+url = "https://dbc-b6c4ca60-2788.cloud.databricks.com/api/2.0/clusters/spark-versions?token=dapi75bb6f2e6e903bf9d18872fcc84f3ba1"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
 
 def main():
 
@@ -15,6 +26,8 @@ def main():
 						help ='sum the integers (default: find the max)')
 
 	args = parser.parse_args()
+ 
+ 
 
 	if args.greet:
 		print("Welcome to GeeksforGeeks !")
